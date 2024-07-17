dd31ae163840d2219e337bcb142ed8fff436963c
Jessica <jessica.gentz@outlook.com>
2024-07-17 15:38:26 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,15 @@
+//this is my practice file
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 5));
+
+
+const subtract = (a, b) => a - b;
+
+console.log(subtract(8, 2));
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(2, 2));
+
